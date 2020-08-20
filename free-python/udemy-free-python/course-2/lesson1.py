e1edb2088c6c52658f33a27ef398a6c639062315
gitdxb <trantambls@gmail.com>
2020-08-20 18:17:23 +0400

    data structure 1

--- free-python/udemy-free-python/course-2/lesson1.py
+++ free-python/udemy-free-python/course-2/lesson1.py
@@ -114,6 +114,67 @@ b = 7
 print(a + str(b))
 
 
+#Working with list
+list1 = [1,2,3,4,5,5.6,"Asma", True]
+print(list1)
+print(list1[1])
+print(list1[1:4])  #only print from index 1 to 3
+print(len(list1))
+
+#Calculate the average
+a = [1,2,3,4,5,6]
+print(sum(a)/len(a)) #output: 3.5
+
+#Manipulate list
+list_one = [1,2,3,4,5,6,"JS", "C++"]
+list_one.append("Python")
+list_one.append(10)
+list_one.insert(1,11) #insert no 11 to position 1
+list_one[1] = 14
+list_one.remove("JS")
+#list_one.pop() => delete last item
+#print(list_one.pop()) => bring a item back
+#list_one.pop(1) , delete item index 1
+print(list_one)
+
+# List and range method
+list1 = list(range(1,11))
+#Reverse range: list2 = list(range(30,1,-2)
 
 
+list1 += [3,223,21,432]
 
+#min print(min(list1)) ====> if the list only contain number or string, not mix
+#max print(max(list1))
+
+#to sort
+list1.sort()
+#or
+list1.sort(reverse=True)
+list1.reverse()
+print(list1)
+
+#List sort and reverse
+data = ["abc", "basda", "cefd","da","e"]
+result = reversed(data) #make a copy and in reverse order, otherwise data.reverse()
+
+
+print(data)
+print(list(result))  #reverse a list
+
+#Sorting
+data = ["abc", "basda", "cefd","da","e"]
+#data.sort()
+
+#or
+result = list(sorted(data))
+
+print(data)
+print(result)
+
+#SOrt by length and reverse
+data = ["abc", "basda", "cefd","da","e"]
+
+result = list(sorted(data, key=len, reverse=True))
+
+print(result)
