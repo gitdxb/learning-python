98bbd73ceb7636978f1aef444bf23bebf3a5cf36
gitdxb <trantambls@gmail.com>
2020-08-25 01:28:19 +0400

    update sandbox 1

---
+++ free-python/sandbox-1.py
@@ -0,0 +1,13 @@
+name = input('Enter your ID: ')
+password = input('Enter your password: ')
+if name == 'Mary':
+  print('Hello, Mary')
+  if password=='swordfish':
+      print('Access granted.')
+  else:
+      print('Access denied')
+else:
+  print('Access denied')
+
+
+
