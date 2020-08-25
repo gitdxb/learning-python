d3e29aa9585f9ddec1b6a5cda6d66c8e11e6cd46
gitdxb <trantambls@gmail.com>
2020-08-26 01:16:25 +0400

    update new sandbox

--- free-python/sandbox2.py
+++ free-python/sandbox2.py
@@ -21,8 +21,38 @@ def addtwo(a,b):
     added = a + b
     return added
 x = addtwo(2,3)
-print(x)
+#print(x)
+get_hours = input("Enter hours: ")
+get_rate = input("Enter rate: ")
+try:
 
+    hours = float(get_hours)
+    rate = float(get_rate)
+except:
+    print("Please enter numeric input")
+
+def computepay(hours,rate):
+    if hours > 40:
+        reg = 40 * rate
+        otp = (hours - 40) * 15
+        pay = reg + otp
+        print("Your pay is: ",pay)
+    else:
+        pay = hours * rate
+        print("Your pay: ",pay)
+# computepay(hours, rate)
+
+# Greeting
+def greet(lang):
+    if lang == 'es':
+        return 'Hola'
+    elif lang == 'fr':
+        return 'Bonjour'
+    else:
+        return 'Hello'
+
+
+print(greet('fr'), 'Sally')
 
 
 
