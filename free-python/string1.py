4a4ab1b11693034a69071f08255d0513f38def10
gitdxb <trantambls@gmail.com>
2020-08-28 13:22:51 +0400

    update String

---
+++ free-python/string1.py
@@ -0,0 +1,21 @@
+# String loop
+'''
+_list1 = ['abcdefgh',1,2,3,'abc',10]
+index = 0
+
+while index < len(_list1):
+    letter = _list1[index]
+    print(letter)
+    index = index + 1
+'''
+
+_list2 = [1,2,3,'hi',20,'John']
+
+index = -1
+while index < len(_list2):
+    letter = _list2[index]
+    print(letter)
+    index = index - 1
+
+
+
