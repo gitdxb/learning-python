95452862742810e12aca56bdd1fe0e44791ef017
gitdxb <trantambls@gmail.com>
2020-12-22 11:58:59 +0400

    new exes

---
+++ ATBS_my_code/strip0.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+spam = 'SpamSpamBaconSpamEggsSpamSpam'
+spam.strip('Spam')
+
+
+# In[2]:
+
+
+ord('H')
+
+
+# In[3]:
+
+
+ord('I')
+
+
+# In[4]:
+
+
+ord('!')
+
+
+# In[5]:
+
+
+ord('A') < ord('B')
+
+
+# In[ ]:
+
+
+
+
