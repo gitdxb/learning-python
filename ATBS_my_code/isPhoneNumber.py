2fdad376ae2732c6e2b55bfe947a2539d9bbac7d
Tam-XU <trantambls@gmail.com>
2021-01-03 15:53:00 +0400

    regexes 0 update

--- ATBS_my_code/isPhoneNumber.py
+++ ATBS_my_code/isPhoneNumber.py
@@ -1,95 +1,32 @@
-{
- "metadata": {
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.5-final"
-  },
-  "orig_nbformat": 2,
-  "kernelspec": {
-   "name": "python38564bit7e0d748942634174a7df9a61ee12db31",
-   "display_name": "Python 3.8.5 64-bit",
-   "language": "python"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2,
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def isPhoneNumber(text):\n",
-    "    if len(text) != 12:\n",
-    "        return False\n",
-    "    for i in range(0,3):\n",
-    "        if not text[i].isdecimal():\n",
-    "            return False\n",
-    "    if text[3] != '-':\n",
-    "        return False\n",
-    "    for i in range(4,7):\n",
-    "        if not text[i].isdecimal():\n",
-    "            return False\n",
-    "    if text[7] != '-':\n",
-    "        return False\n",
-    "    for i in range(8,12):\n",
-    "        if not text[i].isdecimal():\n",
-    "            return False\n",
-    "    return True\n",
-    "\n",
-    "    print('Is 415-555-4242 a phone number?')"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "metadata": {},
-   "outputs": [
-    {
-     "output_type": "stream",
-     "name": "stdout",
-     "text": [
-      "True\n"
-     ]
-    }
-   ],
-   "source": [
-    "print(isPhoneNumber('415-555-4242'))  "
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 4,
-   "metadata": {},
-   "outputs": [
-    {
-     "output_type": "stream",
-     "name": "stdout",
-     "text": [
-      "Is Moshi moshi a phone number?\nFalse\n"
-     ]
-    }
-   ],
-   "source": [
-    "print('Is Moshi moshi a phone number?')\n",
-    "print(isPhoneNumber('Moshi moshi'))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ]
-}
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+def isPhoneNumber(text):
+        if len(text) != 12:
+            return False
+        for i in range(0,3):
+            if not text[i].isdecimal():
+                return False
+        if text[3] != '-':
+            return False
+        for i in range(4,7):
+            if not text[i].isdecimal():
+                return False
+        if text[7] != '-':
+            return False
+        for i in range(8,12):
+            if not text[i].isdecimal():
+                return False
+        return True
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office'
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print('Phone number found: ' + chunk)
+print('Done')
+
+
+# %%
+
+
+
